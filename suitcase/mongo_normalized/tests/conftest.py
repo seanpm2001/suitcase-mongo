dcfac69d283dff3d75f78c47757a6ec2d84551e3
Dan Allan <dallan@bnl.gov>
2024-04-01 10:13:30 -0400

    Expose fixture in conftest

--- suitcase/mongo_normalized/tests/conftest.py
+++ suitcase/mongo_normalized/tests/conftest.py
@@ -2,4 +2,4 @@ from bluesky.tests.conftest import RE  # noqa
 from ophyd.tests.conftest import hw  # noqa
 from suitcase.utils.tests.conftest import (  # noqa
     example_data, generate_data, plan_type, detector_list, event_type)
-from .fixtures import db_factory  # noqa
+from .fixtures import db_factory, db_factory_no_indexes  # noqa
